4ac6f8a6e9ff2d9adab4a9206b789be15d9109dc
Takanori Matsumoto <matscube@gmail.com>
2015-03-12 01:07:10 +0900

    [WIP]

---
+++ app/controllers/contests_controller.rb
@@ -0,0 +1,23 @@
+class ContestsController < ApplicationController
+  layout 'master'
+  def index
+  end
+
+  def submit
+  	@sample = Hoge.new
+  	@sample.title = 'hoge'
+  end
+
+  def list
+  end
+
+  def show
+  end
+end
+
+class Hoge
+	attr_accessor :title, :label
+	def model_name
+		'Hoge'
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
+  get 'contests/index'
+
+  get 'contests/submit'
+
+  get 'contests/list'
+
+  get 'contests/show'
+
+  get 'contest/index'
+
+  get 'contest/submit'
+
+  get 'contest/list'
+
+  get 'contest/show'
+
   resources :samples
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'contests#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
