5c538887847dd106a2eb0fd2cef1ed6ac8ac8170
Takanori Matsumoto <matscube@gmail.com>
2015-03-31 02:00:22 +0900

    Add unicorn file

---
+++ config/unicorn.rb
@@ -0,0 +1,29 @@
+application = 'blinder_contest'
+rails_root = File.expand_path('../../', __FILE__)
+
+worker_processes 2
+working_directory rails_root
+
+listen "#{rails_root}/tmp/unicorn_#{application}.sock"
+stderr_path "#{rails_root}/log/unicorn_error.log"
+stdout_path "#{rails_root}/log/unicorn.log"
+
+
+shared_path = '/var/www/#{application}/shared'
+pid "#{shared_path}/pids/unicorn_#{application}.pid"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
