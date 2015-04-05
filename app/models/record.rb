820dfc98fb1b7eaae9026fc46e5ce6418ae3dd51
Takanori Matsumoto <matscube@gmail.com>
2015-04-06 03:20:20 +0900

    Add time validate method.

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,13 @@
 class Record < ActiveRecord::Base
+	def self.validate_time_string time
+		# 0.99, 59.01
+		if time =~ /^([0-9]|[1-5][0-9]).[0-9][0-9]$/
+			return true
+		# 1:00.00, 59:00.00
+		elsif time =~ /^([1-9]|[1-5][0-9]):[0-5][0-9].[0-9][0-9]$/
+			return true
+		else
+			return false
+		end
+	end
 end
