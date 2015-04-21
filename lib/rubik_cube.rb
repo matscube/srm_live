1737609fafaaec7674c339715297f7e3f994ee25
Takanori Matsumoto <matscube@gmail.com>
2015-04-22 01:48:16 +0900

    Add rotation list

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -1,4 +1,5 @@
 class RubikCube
+	@@ROTATIONS = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
 	def self.get_scramble
 		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
 	end
