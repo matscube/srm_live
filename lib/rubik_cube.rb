c7f67b6d8a327fd1ab333bc6d30f79ff0597a8f0
Takanori Matsumoto <matscube@gmail.com>
2015-04-24 08:51:56 +0900

    Add param to get_scramble

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -1,6 +1,6 @@
 class RubikCube
 	@@ROTATIONS = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
-	def self.get_scramble
+	def self.get_scramble size
 		sz = @@ROTATIONS.count
 		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
 	end
