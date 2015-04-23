32ad185db9b468283458db5570ac03f545492deb
Takanori Matsumoto <matscube@gmail.com>
2015-04-23 09:03:53 +0900

    Count rotations

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -1,6 +1,7 @@
 class RubikCube
 	@@ROTATIONS = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
 	def self.get_scramble
+		sz = @@ROTATIONS.count
 		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
 	end
 end
