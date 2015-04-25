7654f1992b11dc82efa91c6891409c62dd293ac3
Takanori Matsumoto <matscube@gmail.com>
2015-04-25 21:27:26 +0900

    Divide rotation

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -1,7 +1,7 @@
 class RubikCube
-	@@ROTATIONS = ["R", "R'", "R2", "L", "L'", "L2", "U", "U'", "U2", "D", "D'", "D2", "F", "F'", "F2", "B", "B'", "B2"]
+	@@ROTATION_TYPES = ["R", "L", "U", "D", "F", "B"]
+	@@ROTATION_TURNS = ["", "'", "2"]
 	def self.get_scramble size
-		sz = @@ROTATIONS.count
 		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
 	end
 end
