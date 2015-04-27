34a682baa66b0fddfd433c872e9d0de391003d91
Takanori Matsumoto <matscube@gmail.com>
2015-04-27 21:19:45 +0900

    get random rotation type

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -3,7 +3,8 @@ class RubikCube
 	@@ROTATION_TURNS = ["", "'", "2"]
 	def self.get_scramble size
 		(0..size).each do
-
+			rtype_size = @@ROTATION_TYPES.size
+			rtype_id = rand(rtype_size)
 		end
 		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
 	end
