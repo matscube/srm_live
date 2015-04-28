228e93c9ba07becb6cdc09039bbdfb6f2b5e8de1
Takanori Matsumoto <matscube@gmail.com>
2015-04-28 09:05:53 +0900

    Fix get_scramble method

--- lib/rubik_cube.rb
+++ lib/rubik_cube.rb
@@ -2,10 +2,23 @@ class RubikCube
 	@@ROTATION_TYPES = ["R", "L", "U", "D", "F", "B"]
 	@@ROTATION_TURNS = ["", "'", "2"]
 	def self.get_scramble size
+		scramble = []
+		before_rtype_id = -1
 		(0..size).each do
 			rtype_size = @@ROTATION_TYPES.size
 			rtype_id = rand(rtype_size)
+
+			if before_rtype_id == rtype_id
+				rtype_id = (rtype_id + 1) % rtype_size
+			end
+			before_rtype_id = rtype_id
+
+			rturn_size = @@ROTATION_TYPES.size
+			rturn_id = rand(rturn_size)
+
+			turn = @@ROTATION_TYPES[rtype_id].to_s + @@ROTATION_TURNS[rturn_id].to_s
+			scramble.push turn
 		end
-		return "R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R R L' U D2 R L2 B' D2 R"
+		return scramble
 	end
 end
